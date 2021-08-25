24ad8a57a89f6da2a6c9b70d3ddb0bd1edfafe1c
Praduman <praduman2k19@gmail.com>
2021-08-25 22:16:44 +0530

    Updated

--- week-2/Programming_Assignment_2.cpp
+++ week-2/Programming_Assignment_2.cpp
@@ -12,19 +12,26 @@ int main()
         for(int i=0;i<n;i++)
         cin>>arr[i];
 
-        for(int i=1;i<500000;i++)
+        int low=1,minVal=0,temp=0,mid=0;
+        int high=max_element(arr,arr+n)-arr;
+        while(low<=high)
         {
-            long long temp=0;
+            mid=(low+high)/2;
+            temp=0;
             for(int j=0;j<n;j++)
             {
-                 temp+=ceil((arr[j]+0.0)/(i+0.0));
+                 temp+=ceil((arr[j]+0.0)/(mid+0.0));
             }
             if(temp<=m)
             {
-                cout<<i<<endl;
-                break;
+                minVal=mid;
+                high=mid-1;
             }
+            else
+            low=mid+1;
         }
+        cout<<minVal<<endl;
     }
     return 0;
 }
+
